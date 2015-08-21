bf13dc49347d80c0223f1a3bf7b441acf032e9a9
Victor Demin <demmbox@gmail.com>
2015-08-21 10:17:07 +0200

    Create AutoloadExample.php

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ruskid\csvimporter;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
